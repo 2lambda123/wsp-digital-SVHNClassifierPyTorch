d8e571e9b1c6233fc3b02c76f8d61ec58a939f2c
Sushen Satturu <sushen.satturu@wspeonline.com>
2021-05-28 12:32:13 +1000

    MOD-35 Add evaluate methods for least confidence and margin sampling

--- alt_evaluator.py
+++ alt_evaluator.py
@@ -43,3 +43,63 @@ class AltEvaluator(object):
                 results.append(batch_results)
 
         return results
+
+    def evaluate_least_confidence(self, model):
+        results = []
+
+        with torch.no_grad():
+
+            for batch_idx, (images, length_labels, digits_labels, paths) in enumerate(self._loader):
+                images, length_labels, digits_labels = images.cuda(), length_labels.cuda(), [digit_labels.cuda() for
+                                                                                             digit_labels in
+                                                                                             digits_labels]
+                length_logits, digit1_logits, digit2_logits, digit3_logits, digit4_logits, digit5_logits = model.eval()(
+                    images)
+
+                print("Evaluating images in batch: ", batch_idx + 1)
+
+
+                image_logits = [max(digit1_logits.tolist()[0]), max(digit2_logits.tolist()[0]), max(digit3_logits.tolist()[0]), max(digit4_logits.tolist()[0]), max(digit5_logits.tolist()[0])]
+                least_confidence_for_image = min(image_logits)
+
+                # This only makes sense for batch size of 1
+                batch_results = {}
+                for image in paths:
+                    batch_results[image.decode("utf-8")] = {"loss": least_confidence_for_image}
+
+                results.append(batch_results)
+
+        return results
+
+    def evaluate_margin_sampling(self, model):
+        results = []
+
+        with torch.no_grad():
+
+            for batch_idx, (images, length_labels, digits_labels, paths) in enumerate(self._loader):
+                images, length_labels, digits_labels = images.cuda(), length_labels.cuda(), [digit_labels.cuda() for
+                                                                                             digit_labels in
+                                                                                             digits_labels]
+                length_logits, digit1_logits, digit2_logits, digit3_logits, digit4_logits, digit5_logits = model.eval()(
+                    images)
+
+                print("Evaluating images in batch: ", batch_idx + 1)
+
+                image_logits = [self.margin(digit1_logits.tolist()[0]), self.margin(digit2_logits.tolist()[0]),
+                                self.margin(digit3_logits.tolist()[0]), self.margin(digit4_logits.tolist()[0]),
+                                self.margin(digit5_logits.tolist()[0])]
+                smallest_margin = min(image_logits)
+
+
+                # This only makes sense for batch size of 1
+                batch_results = {}
+                for image in paths:
+                    batch_results[image.decode("utf-8")] = {"loss": smallest_margin}
+
+                results.append(batch_results)
+
+        return results
+
+    def margin(self, logits):
+        logits.sort(reverse=True)
+        return logits[0] - logits[1]
